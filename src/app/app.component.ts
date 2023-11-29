18a5a16264f12601c3eba4783c7b0ff50701510b
JoseTorres2210 <86307604+JoseTorres2210@users.noreply.github.com>
2023-11-29 10:12:48 -0500

    Implementacion del html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component'
 import { FooterComponent } from './shared/footer/footer.component'
+import { IndexComponent } from './pages/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
