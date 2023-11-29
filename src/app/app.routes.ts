5061f90a08223ee3fb5755ce65a7fee646892619
Xavier Cobos <xcobos@espol.edu.ec>
2023-11-29 10:40:10 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+
+export const routes: Routes = [
+    { path: '', component: IndexComponent },
+    { path: 'about', component: AboutComponent },
+];
