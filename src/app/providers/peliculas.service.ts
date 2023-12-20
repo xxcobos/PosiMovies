594a07ef2e666e6bc8b878412f61f1026adaf3f7
JoseTorres2210 <86307604+JoseTorres2210@users.noreply.github.com>
2023-12-20 10:03:19 -0500

    Update peliculas.service.ts

--- src/app/providers/peliculas.service.ts
+++ src/app/providers/peliculas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-//Importación del HttpClient
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class PeliculasService {
 
       //Atributo URL
-      private URL: "https://posimovies-a0911-default-rtdb.firebaseio.com/"
+      private URL = "https://posimovies-a0911-default-rtdb.firebaseio.com/" 
 
       //Inyección de dependencia del HttpClient
-      constructor( ... ) { }
+      constructor( private http: HttpClient) { }
 
-      //Método con la petición HTTP
-      getResponse() {
-          return this.http.get(this.URL);
-      }
+  // Método con la petición HTTP GET
+  getResponse(queryParams?: { [key: string]: string }): Observable<any> {
+    let params = new HttpParams();
+    if (queryParams) {
+      Object.keys(queryParams).forEach(key => {
+        params = params.append(key, queryParams[key]);
+      });
+    }
+    return this.http.get<any>(this.URL , { params });
+  }
 
 }
