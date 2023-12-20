31f121b19d46699fe2e05233ee6568025284e533
JoseTorres2210 <86307604+JoseTorres2210@users.noreply.github.com>
2023-12-20 09:38:04 -0500

    Update peliculas.service.ts

--- src/app/providers/peliculas.service.ts
+++ src/app/providers/peliculas.service.ts
@@ -8,6 +8,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PeliculasService {
 
+      //Atributo URL
+      private URL: "https://posimovies-a0911-default-rtdb.firebaseio.com/"
 
+      //Inyección de dependencia del HttpClient
+      constructor( ... ) { }
+
+      //Método con la petición HTTP
+      getResponse() {
+          return this.http.get(this.URL);
+      }
 
 }
