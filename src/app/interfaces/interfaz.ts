161386e7f90d4bcc5282f7cbbd4705508cc6e131
pcolina <86307604+JoseTorres2210@users.noreply.github.com>
2023-12-21 14:19:25 -0500

    footer

--- src/app/interfaces/interfaz.ts
+++ src/app/interfaces/interfaz.ts
@@ -1,2 +1,12 @@
 export interface Interfaz {
+    certificate:string;
+    director:string;
+    genre:string;
+    gross_earn:string;
+    poster:string;
+    rating:string;
+    runtime:string;
+    stars:string;
+    title:string;
+    votes:string;
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from  '@angular/common/http';
+
+
+ //Importación de la interfaz
+ import { Interfaz } from 'mpa/src/app/interfaces/interfaz';
+
+ //Importación del servicio
+ import { PeliculasService } from 'mpa/src/app/providers/peliculas.service'
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  providers: [PeliculasService],
 })
 export class FooterComponent {
+    //Atributo con el tipo de dato de la interfaz
+    public data : Interfaz[] = [];
 
+    //Inyección de dependencia del servicio
+    constructor(private dataProvider: PeliculasService) { }
+
+    //Ejecución de la petición y suscripción de la respuesta
+
+    ngOnInit() {
+      this.dataProvider.getResponse().subscribe((response) => { 
+        let dataArray = (response as Interfaz[]); 
+        this.data = dataArray.slice(0,10);
+      })
+    }
 }
